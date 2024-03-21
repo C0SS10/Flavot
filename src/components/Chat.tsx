31092880d4e3f75eb75ae5c8567e6747a1baa999
C0SS10 <estebancg12@hotmail.com>
2024-03-21 13:50:19 -0500

    🎨Add style to component chat

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -6,15 +6,33 @@ export function Chat() {
   return (
     <section className="chat">
       <header className="chat-header">
-        <h1>Flavot</h1>
+        <h1>Flavot | Ideas for cooking</h1>
         <img src={Hat} alt="Icon chef hat" className="icon" />
       </header>
       <main className="chat-window">
         <ul className="message incoming">
-          <li></li>
+          <li>But the life came to me and i was happy with it</li>
         </ul>
         <ul className="message outgoing">
-          <li></li>
+          <li>All my effort was in vane, but the think i was doing is that</li>
+        </ul>
+        <ul className="message incoming">
+          <li>But the life came to me and i was happy with it</li>
+        </ul>
+        <ul className="message outgoing">
+          <li>All my effort was in vane, but the think i was doing is that All my effort was in vane, but the think i was doing is that</li>
+        </ul>
+        <ul className="message incoming">
+          <li>But the life came to me and i was happy with it</li>
+        </ul>
+        <ul className="message outgoing">
+          <li>All my effort was in vane, but the think i was doing is that</li>
+        </ul>
+        <ul className="message incoming">
+          <li>But the life came to me and i was happy with it</li>
+        </ul>
+        <ul className="message outgoing">
+          <li>All my effort was in vane, but the think i was doing is that</li>
         </ul>
       </main>
       <footer className="chat-footer">
@@ -24,9 +42,9 @@ export function Chat() {
           aria-label="Here your message"
           placeholder="Cook your message"
         />
-        <button type="button" className="send-button">
+        <a href="#" className="send-button">
           <img src={Send} alt="Send icon" className="send-icon" />
-        </button>
+        </a>
       </footer>
     </section>
   );
