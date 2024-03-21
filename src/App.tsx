de7470e864b2e340ff452e6f5f35cf8fcc9c6901
C0SS10 <estebancg12@hotmail.com>
2024-03-21 09:46:24 -0500

    🧰Update import paths and add Chat component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import "./App.css";
+import "@/styles/App.css";
+import { Chat } from "@/components/Chat";
 
 function App() {
   return (
     <>
-      <h1>Hola</h1>
+      <Chat />
     </>
   );
 }
